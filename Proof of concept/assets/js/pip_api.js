052be05e173c79ef0220137beeb92df61dabec60
MichaelCzarRivera <mcmr120198@gmail.com>
2022-06-03 16:42:35 +0400

    all files

---
+++ Proof of concept/assets/js/pip_api.js
@@ -0,0 +1,139 @@
+﻿pipButton.hidden = !document.pictureInPictureEnabled || video.disablePictureInPicture;
+
+pipButton.addEventListener('click', function () {
+
+    if (!document.pictureInPictureElement) {
+        video.requestPictureInPicture()
+            .then(pipWindow => {
+                updateVideoSize(pipWindow.width, pipWindow.height);
+                pipWindow.addEventListener('resize', function (event) {
+                    updateVideoSize(pipWindow.width, pipWindow.height);
+                });
+            })
+            .catch(error => {
+
+            });
+    } else {
+        document.exitPictureInPicture()
+            .catch(error => {
+
+            });
+    }
+});
+
+function updateVideoSize(width, height) {
+
+}
+
+video.addEventListener('enterpictureinpicture', function () {
+
+});
+
+video.addEventListener('leavepictureinpicture', function () {
+
+});
+
+pipButtontwo.hidden = !document.pictureInPictureEnabled || video.disablePictureInPicture;
+
+pipButtontwo.addEventListener('click', function () {
+
+    if (!document.pictureInPictureElement) {
+        videotwo.requestPictureInPicture()
+            .then(pipWindow => {
+                updateVideoSize(pipWindow.width, pipWindow.height);
+                pipWindow.addEventListener('resize', function (event) {
+                    updateVideoSize(pipWindow.width, pipWindow.height);
+                });
+            })
+            .catch(error => {
+
+            });
+    } else {
+        document.exitPictureInPicture()
+            .catch(error => {
+
+            });
+    }
+});
+
+function updateVideoSize(width, height) {
+
+}
+
+video.addEventListener('enterpictureinpicture', function () {
+
+});
+
+video.addEventListener('leavepictureinpicture', function () {
+
+});
+
+pipButtonthree.hidden = !document.pictureInPictureEnabled || video.disablePictureInPicture;
+
+pipButtonthree.addEventListener('click', function () {
+
+    if (!document.pictureInPictureElement) {
+        videothree.requestPictureInPicture()
+            .then(pipWindow => {
+                updateVideoSize(pipWindow.width, pipWindow.height);
+                pipWindow.addEventListener('resize', function (event) {
+                    updateVideoSize(pipWindow.width, pipWindow.height);
+                });
+            })
+            .catch(error => {
+
+            });
+    } else {
+        document.exitPictureInPicture()
+            .catch(error => {
+
+            });
+    }
+});
+
+function updateVideoSize(width, height) {
+
+}
+
+video.addEventListener('enterpictureinpicture', function () {
+
+});
+
+video.addEventListener('leavepictureinpicture', function () {
+
+});
+
+pipButtonfour.hidden = !document.pictureInPictureEnabled || video.disablePictureInPicture;
+
+pipButtonfour.addEventListener('click', function () {
+
+    if (!document.pictureInPictureElement) {
+        videofour.requestPictureInPicture()
+            .then(pipWindow => {
+                updateVideoSize(pipWindow.width, pipWindow.height);
+                pipWindow.addEventListener('resize', function (event) {
+                    updateVideoSize(pipWindow.width, pipWindow.height);
+                });
+            })
+            .catch(error => {
+
+            });
+    } else {
+        document.exitPictureInPicture()
+            .catch(error => {
+
+            });
+    }
+});
+
+function updateVideoSize(width, height) {
+
+}
+
+video.addEventListener('enterpictureinpicture', function () {
+
+});
+
+video.addEventListener('leavepictureinpicture', function () {
+
+});
---
+++ Proof of concept/assets/js/weather_api.js
@@ -0,0 +1,18 @@
+﻿window.weatherWidgetConfig = window.weatherWidgetConfig || [];
+window.weatherWidgetConfig.push({
+    selector: ".weatherWidget",
+    apiKey: "TT5ZUWXBXK7DNN9UM4SBR9KW2", //Sign up for your personal key
+    location: "Dubai, AE", //Enter an address
+    unitGroup: "metric", //"us" or "metric"
+    forecastDays: 5, //how many days forecast to show
+    title: "Dubai, AE", //optional title to show in the 
+    showTitle: true,
+    showConditions: true
+});
+
+(function () {
+    var d = document, s = d.createElement('script');
+    s.src = 'https://www.visualcrossing.com/widgets/forecast-simple/weather-forecast-widget-simple.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+})();
